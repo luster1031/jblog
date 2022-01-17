f12b576511148a7c5ef97e5739408d10b50f5eb0
luster1031 <luster47@naver.com>
2022-01-17 14:55:05 +0900

    el연습 #2

--- servlet-practice/jstel/src/main/java/jstel/_01Servlet.java
+++ servlet-practice/jstel/src/main/java/jstel/_01Servlet.java
@@ -1,6 +1,9 @@
 package jstel;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +12,44 @@ import javax.servlet.http.HttpServletResponse;
 public class _01Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//	메소드 체인 (계속 점 쓰는 것)
+		//	값
+		int iVal= 10;
+		int lVal = 10;
+		float fVal = 3.14f;
+		boolean bVal = true;
+		String sVal = "가나다라마바사";
+		
+		
+		//	객체
+		Object obj = null;
+		UserVo userVo = new UserVo();
+		userVo.setNo(10L);
+		userVo.setName("둘리");
+		
+		
+		//	MAP
+		Map<String, Object> map = new HashMap<>();
+		map.put("ival", iVal);
+		map.put("lval", lVal);
+		map.put("fval", fVal);
+		map.put("bval", bVal);
+		
+		
+		request.setAttribute("ival", iVal);
+		request.setAttribute("lval", lVal);
+		request.setAttribute("fval", fVal);
+		request.setAttribute("bval", bVal);
+		request.setAttribute("sval", sVal);
+		
+		request.setAttribute("obj", obj);	//	object가 null일 때
+		request.setAttribute("user", userVo);
+		
+		
+		request.setAttribute("m", map);
+		
+		
+		
+		//		메소드 체인 (계속 점 쓰는 것)
 		request
 			.getRequestDispatcher("/WEB-INF/views/01.jsp")
 			.forward(request, response);
