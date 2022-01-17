c8116e43b10fb9d9d2a5f4859c9691447940eee1
luster1031 <luster47@naver.com>
2022-01-17 14:24:49 +0900

    el연습 #1 start

---
+++ servlet-practice/jstel/src/main/java/jstel/_01Servlet.java
@@ -0,0 +1,22 @@
+package jstel;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class _01Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//	메소드 체인 (계속 점 쓰는 것)
+		request
+			.getRequestDispatcher("/WEB-INF/views/01.jsp")
+			.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
