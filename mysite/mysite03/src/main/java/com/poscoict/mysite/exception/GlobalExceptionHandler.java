c6747a714e09c2d1bd42fd083a0ab5d881bd1893
luster1031 <luster47@naver.com>
2022-01-26 14:07:12 +0900

    mysite - logback(slf4j) 적용

--- mysite/mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite/mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
+
 package com.poscoict.mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 /*
  * 모든 에러가 여기로 AOP기술
@@ -13,13 +17,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		
 		//	1. 로깅
-		//  errors가 가지고 있는 버퍼 안에 에러 내용 적혀 있음
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors)); 
+		LOGGER.error(errors.toString());
+		
+		//  errors가 가지고 있는 버퍼 안에 에러 내용 적혀 있음
 		System.out.println(errors.toString());
 		
 		model.addAttribute("exception", errors.toString());
