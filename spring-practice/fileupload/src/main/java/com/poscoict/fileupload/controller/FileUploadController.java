4cfcb554af980af78a07874c71dd3f19b876ead3
luster1031 <luster47@naver.com>
2022-01-27 20:02:09 +0900

    fileupload #final

--- spring-practice/fileupload/src/main/java/com/poscoict/fileupload/controller/FileUploadController.java
+++ spring-practice/fileupload/src/main/java/com/poscoict/fileupload/controller/FileUploadController.java
@@ -1,14 +1,20 @@
 package com.poscoict.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.poscoict.fileupload.service.FileUploadService;
+
 @Controller
 public class FileUploadController {
+	@Autowired
+	private FileUploadService fileUploadService;
 	
 	@RequestMapping({"","/form"})
 	public  String form() {
@@ -22,10 +28,12 @@ public class FileUploadController {
 			, Model model) {
 		
 		System.out.println("email : " + email);
+		System.out.println("multipartFile : " + multipartFile);
+		
 		
 		String url = fileUploadService.restore(multipartFile);
+		model.addAttribute("url",url);
 		
-		model.addAttribute("url");
 		return "result";
 	}
 }
