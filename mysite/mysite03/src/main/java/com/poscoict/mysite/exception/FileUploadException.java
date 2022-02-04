0e7c35357b9fa658717295ba06b4563a1783765d
luster1031 <luster47@naver.com>
2022-02-04 11:14:16 +0900

    mysite03 코드 리뷰

--- mysite/mysite03/src/main/java/com/poscoict/mysite/exception/FileUploadException.java
+++ mysite/mysite03/src/main/java/com/poscoict/mysite/exception/FileUploadException.java
@@ -4,7 +4,10 @@ public class FileUploadException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public FileUploadException(String message) {
-		
+		super(message);
 	}
 
+	public FileUploadException() {
+		super("FileUpload Exception Occurs");
+	}
 }
