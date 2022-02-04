1c466d1d3738795c182aaedd2562f6790a9b206c
luster1031 <luster47@naver.com>
2022-02-04 09:27:46 +0900

    [AuthInterceptor] 권한 체크

--- mysite/mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
+++ mysite/mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
@@ -1,17 +1,12 @@
 package com.poscoict.mysite.security;
 
 import javax.servlet.http.HttpServletRequest;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.poscoict.mysite.repository.SiteRepository;
-import com.poscoict.mysite.repository.UserRepository;
-import com.poscoict.mysite.vo.SiteVo;
 import com.poscoict.mysite.vo.UserVo;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter {
@@ -36,15 +31,18 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 		if (auth == null) {
 			// 클래스로 올라가서 찾아봐야 한다.
 			auth = handlerMethod.getBeanType().getAnnotation(Auth.class);
+//			auth = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Auth.class);
 		}
 
 		// 5. type과 method에 @Auth가 적용이 안 되어 있는 경우
-		if (auth == null) { // 인증이 필요없는 것이라면
-			System.out.println("인증이 필요 없");
+		// 인증이 필요없는 것이라면
+		if (auth == null) { 
 			return true;
 		}
 
-		// 5 @Auth가 적용이 되어 있기 때문에, 인증(Authentication) 여부 확인
+//	--------------인증이 필요함 ---------------------------
+		
+		//	6. @Auth가 적용이 되어 있기 때문에, 인증(Authentication) 여부 확인
 		HttpSession session = request.getSession();
 		if (session == null) {
 			response.sendRedirect(request.getContextPath() + "/user/login");
@@ -53,25 +51,29 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
 		// 세션이 있는 것
 		UserVo authUser = (UserVo) session.getAttribute("authUser");
-		System.out.println("[세션이 있는 것 : ]");
 		if (authUser == null) {
 			response.sendRedirect(request.getContextPath() + "/user/login");
 			return false;
 		}
 
-		// authInterceptor role처리
+		//	7. 권한(Authorization) 체크를 위해서 @Auth의 role가져오기 ("USER", "ADMIN")
 		String role = authUser.getRole();
-		System.out.println("[role] : " + role);
-		System.out.println("[authUser] : " + authUser.toString());
-		
-		if ("ADMIN".equals(role)) {
+
+		//	8. @Auth의 role이 "USER"인 경우, authUser의 role은 상관이 없다. 
+		// authInterceptor role처리
+		if ("USER".equals(role)) {
 			return true;
 		}
-		if(!auth.role().equals(authUser.getRole())) {
+
+		//	9. @Auth의 roler이 "ADMIN"인 경우, authUser은 "ADMIN"이어야 한다. 
+		if ("ADMIN".equals(authUser.getRole())==false) {
 			response.sendRedirect(request.getContextPath());
 			return false;
 		}
-		// 6. 인증 확인 --> controller의 handler(methode) 실행
+
+		// 	10. 옳은 관리자
+		//	@Auth의 role: "ADMIN"
+		//	authUser의 role : "ADMIN"
 		return true;
 	}
 
