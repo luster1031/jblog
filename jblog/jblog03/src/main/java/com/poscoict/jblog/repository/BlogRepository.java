a5c7a4d5c62111f8e75350ab21bd85bdcd2f26e9
luster1031 <luster47@naver.com>
2022-02-06 14:58:16 +0900

    [jblog03-1] join(회원가입)-추가) default logo를 /assets 경로에 해줬다.

--- jblog/jblog03/src/main/java/com/poscoict/jblog/repository/BlogRepository.java
+++ jblog/jblog03/src/main/java/com/poscoict/jblog/repository/BlogRepository.java
@@ -17,13 +17,17 @@ public class BlogRepository {
 	public void DefaultContent(String id) {
 		Map<String, String> map = new HashMap<>();
 		map.put("title", "환영합니다.자신만의 글을 적어보세요!");
-		map.put("logo", "/images/202214104258374.jpg");
-		map.put("id", id);
+		map.put("logo", "/assets/images/aa.jpg");
+		map.put("user_id", id);
 		sqlSession.insert("blog.default", map);
 	}
 
 	public BlogVo getContent(String id) {
 		return sqlSession.selectOne("blog.front",id);
+	}
+
+	public boolean updateContent(BlogVo blogvo) {
+		return sqlSession.update("blog.update",blogvo)==1;
 	}
 
 }
