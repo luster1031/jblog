936afb485065e18ee5e53094a457eb9f6a3194f3
luster1031 <luster47@naver.com>
2022-02-11 17:34:45 +0900

    [jblog06] ADD BootInitializer

--- jblog/jblog06/src/main/java/com/poscoict/jblog/JblogApplication.java
+++ jblog/jblog06/src/main/java/com/poscoict/jblog/JblogApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysiteApplication {
+public class JblogApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MysiteApplication.class, args);
+		SpringApplication.run(JblogApplication.class, args);
 	}
 }
